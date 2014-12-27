d2371dd3c30b907561a911a86c33ea326c691cee
Sami Kerola <kerolasa@iki.fi>
2014-12-27 09:32:45 +0000

    script: replace strftime() workaround with CFLAGS = -Wno-format-y2k
    Nowadays, gcc(1) provides the -Wno-format-y2k option to prevent the
    warning, so that the above workaround is no longer required.
    
    Reference: http://man7.org/linux/man-pages/man3/strftime.3.html
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- term-utils/script.c
+++ term-utils/script.c
@@ -149,15 +149,6 @@ static void die_if_link(const struct script_control *ctl)
 		       "Program not started."), ctl->fname);
 }
 
-/*
- * Stop extremely silly gcc complaint on %c:
- *  warning: `%c' yields only last 2 digits of year in some locales
- */
-static void my_strftime(char *buf, size_t len, const char *fmt, const struct tm *tm)
-{
-	strftime(buf, len, fmt, tm);
-}
-
 static void __attribute__((__noreturn__)) done(struct script_control *ctl)
 {
 	if (ctl->isterm)
@@ -291,7 +282,7 @@ static void do_io(struct script_control *ctl)
 	pfd[2].fd = ctl->sigfd;
 	pfd[2].events = POLLIN | POLLERR | POLLHUP;
 
-	my_strftime(buf, sizeof buf, "%c\n", localtime(&tvec));
+	strftime(buf, sizeof buf, "%c\n", localtime(&tvec));
 	fprintf(ctl->typescriptfp, _("Script started on %s"), buf);
 
 	while (!ctl->die) {
